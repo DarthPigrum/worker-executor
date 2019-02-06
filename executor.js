0c362c813b57ff80c1d570ac294aad90023626d2
DarthPigrum <43322390+DarthPigrum@users.noreply.github.com>
2019-02-07 00:00:34 +0200

    Fix of path to worker.js

--- executor.js
+++ executor.js
@@ -1,10 +1,13 @@
 'use strict';
 const { Worker } = require('worker_threads');
+const libPATH = module.filename.split('/');
+libPATH.pop();
+const workerPATH = libPATH.join('/') + '/worker.js';
 module.exports = class WorkerPool {
   constructor(size = 0) {
     this.workers = [];
     this.allowSpawn = false;
-    for (let n = 0; n < size; n++) this.workers.push(new Worker('./worker.js'));
+    for (let n = 0; n < size; n++) this.workers.push(new Worker(workerPATH));
   }
   run(fn, ...args) {
     if (this.workers.length === 0 && !this.allowSpawn) {
@@ -13,7 +16,7 @@ module.exports = class WorkerPool {
       return { promise };
     }
     const fromPool = (this.workers.length !== 0);
-    const worker = fromPool ? this.workers.pop() : new Worker('./worker.js');
+    const worker = fromPool ? this.workers.pop() : new Worker(workerPATH);
     worker.postMessage([fn.toString(), ...args]);
     let done = false;
     const promise = new Promise((resolve, reject) => {
@@ -27,7 +30,7 @@ module.exports = class WorkerPool {
       if (!done) {
         done = true;
         worker.terminate();
-        if (fromPool) this.workers.push(new Worker('./worker.js'));
+        if (fromPool) this.workers.push(new Worker(workerPATH));
         return true;
       } else { return false; }
     };
